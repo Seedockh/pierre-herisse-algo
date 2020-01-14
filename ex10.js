849d0a4309ef0d1af47245fd0c3a994d0ee15c46
Seedockh <pierre.herisse@gmail.com>
2020-01-14 15:13:56 +0100

    object oriented design ok

---
+++ ex10.js
@@ -0,0 +1,36 @@
+module.exports = class Deck {
+  constructor() {
+    this.deckLimit = 52
+    this.maxCardsPerColor = 13
+    this.colors = ["club", "diamond", "heart", "spade"]
+    this.nbColors = this.colors.length
+    this.cardNumbers = [...Array(10).keys()].map(el => el+1)
+    this.cardHeadsNames = ["jack", "queen", "king"]
+    this.cardHeadsValues = [11, 12, 13]
+    this.cards = {}
+  }
+
+  sortDeck() {
+    this.colors.map( color => {
+      let nbVal
+      this.cardNumbers.map( nb => {
+        if (nb === 1) nbVal = 14
+        else nbVal = nb
+        this.cards[`${color}-${nb}`] = {
+          value: nbVal, color: color,
+        }
+      })
+      this.cardHeadsNames.map( (headName,index) => {
+        this.cards[`${color}-${headName}`] = {
+          value: this.cardHeadsValues[index], color: color,
+        }
+      })
+    })
+  }
+
+  // For example deck.battle('club-9', 'spade-king')
+  battle(card1, card2) {
+    if (this.cards[card1].value > this.cards[card2].value) return card1
+    return card2
+  }
+}
---
+++ ex11.js
@@ -0,0 +1,69 @@
+class ChessGame {
+  constructor() {
+    this.cheesmenPerSide = { king: 1, queen: 1, rooks: 2, bishops: 2, knights: 2, pawns: 8 }
+    this.chessboard = this.createBoard()
+  }
+
+  createBoard () {
+    let board = new Array(8)
+    // for each horizontal line
+    for (let i=0; i<8; i++) {
+      board[i] = new Array(8)
+      // create one column 8 blocks high
+      for (let j=0; j<8; j++) {
+        board[i][j] = {
+          chessman: null,
+          color: (j+i+2)%2===0 ? 'white' : 'black'
+        }
+      }
+    }
+    return board
+  }
+
+  getDefaultPos(index) {
+    let currentPiece = null
+    switch(index) {
+      case 0: currentPiece = 'rook'
+        break
+      case 7: currentPiece = 'rook'
+        break
+      case 1: currentPiece = 'knight'
+        break
+      case 6: currentPiece = 'knight'
+        break
+      case 2: currentPiece = 'bishop'
+        break
+      case 5: currentPiece = 'bishop'
+        break
+      case 3: currentPiece = 'queen'
+        break
+      case 4: currentPiece = 'king'
+        break
+      default: null
+    }
+    return currentPiece
+  }
+
+  setWhiteChessMen () {
+    for (let i=0; i<this.cheesmenPerSide.pawns; i++) {
+      this.chessboard[i][0].chessman = this.getDefaultPos(i)
+      this.chessboard[i][1].chessman = 'pawn'
+    }
+  }
+
+  setBlackChessMen () {
+    for (let i=0; i<this.cheesmenPerSide.pawns; i++) {
+      this.chessboard[i][6].chessman = 'pawn'
+      this.chessboard[i][7].chessman = this.getDefaultPos(i)
+    }
+  }
+
+  printBoard () {
+    console.log(this.chessboard)
+  }
+}
+
+const chess = new ChessGame()
+chess.setWhiteChessMen()
+chess.setBlackChessMen()
+chess.printBoard()
