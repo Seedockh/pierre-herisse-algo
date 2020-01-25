740839e7772154310ed285eb86c9fb9ff2035999
Seedockh <pierre.herisse@gmail.com>
2020-01-25 05:10:19 +0100

    init ex17

--- ex17.js
+++ ex17.js
@@ -1,7 +1,47 @@
+class Tree {
+  constructor() {
+    this.tree = []
+  }
 
-/*
-import Tree from "./ex17"
-​
-const t = new Tree<number>();
-t.addNode(42);
-*/
+  add(value, parent) {
+    if (this.tree[`${parent}`])
+      this.tree[`${parent}`].children.push(value)
+
+    this.tree[`${value}`] = { v: value, children: [], parent: parent }
+  }
+
+  find(value) {
+    console.log(this.tree[value]);
+    return this.tree[value]
+  }
+
+  deleteNode(value) {
+    this.tree.forEach( branch => {
+      console.log(branch)
+      if (branch.children.indexOf(value) > -1) {
+        console.log(`deleting ${value} from ${branch.v} children`);
+        delete branch.children[value]
+      }
+
+      if (branch.parent === value) {
+        console.log(`deleting child ${branch.v} from ${value}`);
+        delete this.tree[branch.v]
+      }
+    })
+
+    delete this.tree[value]
+  }
+}
+
+const t = new Tree()
+t.add(1)
+t.add(10, 1)
+t.add(11, 10)
+t.add(110, 11)
+t.add(100, 1)
+t.add(101, 100)
+t.find(1)
+console.log('----- DELETE')
+t.deleteNode(11)
+console.log(t)
+t.find(10)
