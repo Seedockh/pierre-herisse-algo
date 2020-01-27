379bc7037cbf3c880a4261ca8459358db29329d3
Seedockh <pierre.herisse@gmail.com>
2020-01-27 08:38:57 +0100

    remove useless comments/logs

--- ex20.js
+++ ex20.js
@@ -1,25 +1,19 @@
 const Graph = require('./ex16.js')
 
-// Depth First Search:
-// involves searching a node and all its children before proceed- ing to its siblings
 function depthFirstSearch(graph, value) {
   let result = null
   graph.adjencies.forEach(children =>
     result ? result :
     children.map(child => child === value ? result = graph.adjencies.get(child) : null)
   )
-  if (result) console.log('----> result found in depth')
   return result ? result : breadthFirstSearch(graph, value)
 }
 
-// Breadth First Search:
-// involves searching a node and its siblings before going on to any children
 function breadthFirstSearch(graph, value) {
   let result = null
   graph.adjencies.forEach((children, key) =>
     result ? result : key === value ? result = graph.adjencies.get(value) : null
   )
-  if (result) console.log('----> result found in breadth')
   return result ? result : depthFirstSearch(graph, value)
 }
 
@@ -39,9 +33,9 @@ g.addEdge(8, 42)
 //g.dump()
 
 console.log('=== SEARCHING ELEMENT WITH EDGES ===')
-console.log(`values in key 8 : ${depthFirstSearch(g, 8)}`)
-console.log(`values in key 8 : ${breadthFirstSearch(g, 8)}`)
+console.log(`values in key 8 :`, depthFirstSearch(g, 8))
+console.log(`values in key 8 :`, breadthFirstSearch(g, 8))
 console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
 console.log('=== SEARCHING ELEMENT WITHOUT EDGES ===')
-console.log(`values in key 20 : ${depthFirstSearch(g, 20)}`)
-console.log(`values in key 20 : ${breadthFirstSearch(g, 20)}`)
+console.log(`values in key 20 :`, depthFirstSearch(g, 20))
+console.log(`values in key 20 :`, breadthFirstSearch(g, 20))
